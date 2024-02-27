49efc35d10ee92775bec22c5143ec437bf460e50
nwishne <natewishne@gmail.com>
2024-02-26 20:50:41 -0600

    Lab_3_V1

--- cyclic.py
+++ cyclic.py
@@ -2,7 +2,14 @@ from llist import *
 
 def llprint(lst, num):
     """print the first num terms of linked list lst"""
-    pass
+    if lst.head:
+        node = lst.head
+        print(node.val)
+        while node.next:
+            node = node.next
+            print(node.val)
+    else:
+        print(lst.head)
 
 def find_cycle(lst):
     """return the start index and length of the cycle"""
--- finite.py
+++ finite.py
@@ -3,18 +3,43 @@ from llist import LList, Node, append
 
 def length(lst):
     """return the length of a finite linked list"""
-    pass  # delete this line when you add your code
-
+    count = 0
+    if lst.head:
+        node = lst.head
+        count = 1
+        while node.next:
+            count += 1
+            node = node.next
+    else:
+        count = 0
+    print(count)
 
 def llprint(lst):
     """print a finite linked list"""
-    pass  # delete this line when you add your code
-
+    
+    if lst.head:
+        node = lst.head
+        print(node.val)
+        while node.next:
+            node = node.next
+            print(node.val)
+    else:
+        print(lst.head)
 
 if __name__ == "__main__":
 
-    pass  # delete this line when you add your code below
-
-    #
-    # your tests go here
-    #
+    llist = LList()
+    append(llist, Node(1))
+    append(llist, Node(2))
+    append(llist, Node(4))
+    append(llist, Node(8))
+    append(llist, Node(16))
+    append(llist, Node(32))
+    append(llist, Node(64))
+    append(llist, Node(128))
+    append(llist, Node(256))
+    append(llist, Node(512))
+    print("This works right")
+    length(llist)
+    print('List:')
+    llprint(llist)
